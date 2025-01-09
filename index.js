dc94b598a96480ac46c9cb38691a2cc53d671e7a
Sujit Kumar <sujitkymar101@gmail.com>
2025-01-10 01:46:53 +0530

    remove comment

--- index.js
+++ index.js
@@ -2,19 +2,9 @@ const express = require("express");
  const connectDB= require('./config/db');
 const app = express();
 const cors = require("cors");
-// const PORT = 3000;
+
 require('dotenv').config();
 const authRoutes = require("./routes/authRoutes");
-// const URI ="mongodb+srv://sujitkymar101:sujitkymar101@cluster0.7hkoe.mongodb.net/";
-// const URI = process.env.MONGO_URI;
-// MongoDB connection
-// mongoose.connect(URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverSelectionTimeoutMS: 5000, // 5 seconds timeout
-// })
-// .then(() => console.log("Database connected successfully"))
-// .catch((err) => console.error("Database connection error:", err));
 
 connectDB();
 // Middleware
